f89ae2a59f3d959a4c2fa649e35b324f59eed105
akashbiradar99 <akashbiradar99@gmail.com>
2023-12-29 18:21:52 +0530

    added changes for scanner class with All datatype examples

--- src/com/dsa/control/statements/ScannerClassExample.java
+++ src/com/dsa/control/statements/ScannerClassExample.java
@@ -7,20 +7,40 @@ public class ScannerClassExample {
     //How to accept different types of data using Scanner class.?
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Number");
-        int num = sc.nextInt();
-        System.out.println("You have entered : "+num);
+        // Reading integer
+        System.out.print("Enter an integer: ");
+        int intValue = scanner.nextInt();
+        System.out.println("You entered an integer: " + intValue);
 
-        System.out.println("Enter Name");
-        String name = sc.next();
-        System.out.println("You have entered : "+name);
+        // Reading double
+        System.out.print("Enter a double: ");
+        double doubleValue = scanner.nextDouble();
+        System.out.println("You entered a double: " + doubleValue);
 
+        // Reading boolean
+        System.out.print("Enter a boolean (true/false): ");
+        boolean boolValue = scanner.nextBoolean();
+        System.out.println("You entered a boolean: " + boolValue);
 
+        // Reading string
+        System.out.print("Enter a string: ");
+        String stringValue = scanner.next();
+        System.out.println("You entered a string: " + stringValue);
+
+        // Reading a line of text
+        System.out.print("Enter a line of text: ");
+        String lineValue = scanner.nextLine();
+        System.out.println("You entered a line of text: " + lineValue);
+
+        // Reading a char (as a String)
+        System.out.print("Enter a character: ");
+        String charInput = scanner.next();
+        char charValue = charInput.charAt(0);
+        System.out.println("You entered a character: " + charValue);
+
+        // Close the scanner
+        scanner.close();
     }
-
-
-
-
 }
