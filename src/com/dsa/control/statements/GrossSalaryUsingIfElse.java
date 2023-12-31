95f1da5a46101ded3ff6a7ced51c6ab65defe666
akashbiradar99 <akashbiradar99@gmail.com>
2023-12-31 13:41:18 +0530

    added changes for gross salary using ifElse examples

--- src/com/dsa/control/statements/GrossSalaryUsingIfElse.java
+++ src/com/dsa/control/statements/GrossSalaryUsingIfElse.java
@@ -3,6 +3,11 @@ package com.dsa.control.statements;
 import java.util.Scanner;
 
 public class GrossSalaryUsingIfElse {
+
+    //Write a program to input basic salary of an employee and calculate its Gross salary according to following:
+    //(Variations)
+    //Basic Salary <= 10000: HRA = 20%, DA =80% Basic Salary <= 20000 : HRA = 25%, DA=90% Basic Salary >
+    //20000 : HRA = 30%, DA =95%
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
