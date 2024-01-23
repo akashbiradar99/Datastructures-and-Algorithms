0d3bba9dfade16c1a6fd46744ea20f2dd618eb72
akashbiradar99 <akashbiradar99@gmail.com>
2024-01-24 05:00:44 +0530

    console print added in example

--- src/com/dsa/control/statements/ConsolePrintExample.java
+++ src/com/dsa/control/statements/ConsolePrintExample.java
@@ -3,8 +3,9 @@ package com.dsa.control.statements;
 public class ConsolePrintExample {
     public static void main(String[] args) {
         // Print the messages on the console
-        System.out.println("1) Hef \n Shine");
-        System.out.println("2) \"HefShine\"");
+        System.out.println("1) Akash\" Biradar");
+        System.out.println("2) \"Akash\"");
+        System.out.println("3) Akash \n Biradar");
     }
 }
 
